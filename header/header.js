d1e18661793887f33cfe30e0b85e90f1b5eddb6e
Pedro Vinicius <129136825+zebedelu@users.noreply.github.com>
2025-07-14 16:41:50 -0300

    Add files via upload

---
+++ header/header.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+    document.body.insertAdjacentHTML("afterbegin", 
+        `
+<header>
+    <img id="menu" src="header/menu.png" alt="Menu">
+    <img id="logo" src="favicon.ico" alt="Logo" >
+    <h1>Artephy</h1>
+</header>
+<div id="background-black"></div>
+<div id="BarraMenu">
+     <img id="LogoMenu" src="favicon.ico" alt="Logo">
+    <h1>Artephy</h1>
+    <ul>
+        <li><a href="home.html">HOME</a></li>
+        <li><a href="projects.html">SEUS PROJETOS</a></li>
+        <li><a href="sobre.html">SOBRE</a></li>
+        <li><a href="tutorial.html">TUTORIAL</a></li>
+    </ul>
+</div>
+`);
+})
