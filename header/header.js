0f3398a641aeb0ac079572715dd563e3e807e609
Pedro Vinicius <129136825+zebedelu@users.noreply.github.com>
2025-08-11 15:35:06 -0300

    Add files via upload

--- header/header.js
+++ header/header.js
@@ -1,21 +1,37 @@
-document.addEventListener("DOMContentLoaded", ()=>{
-    document.body.insertAdjacentHTML("afterbegin", 
+document.addEventListener("DOMContentLoaded", () => {
+  document.body.insertAdjacentHTML(
+    "afterbegin",
+    `
+    <header id="DefaultHeader">
+        <img id="menu" src="header/menu.png" alt="Menu">
+        <img id="logo" src="favicon.ico" alt="Logo" >
+        <h1>Artephy</h1>
+    </header>
+    <div id="background-black"></div>
+    <div id="BarraMenu">
+        <img id="LogoMenu" src="favicon.ico" alt="Logo">
+        <h1>Artephy</h1>
+        <ul>
+            <li><a href="home.html">HOME</a></li>
+            <li><a href="home.html#sobre">SOBRE</a></li>
+            <li><a href="projects.html">SEUS PROJETOS</a></li>
+            <li><a href="tutorial.html">TUTORIAL</a></li>
+        </ul>
+    </div>
+    `,
+    document.body.querySelector("nav").insertAdjacentHTML(
+      "beforeend",
+      `
+        <footer>
+            <nav>
+                <a href="home.html">Home</a> • 
+                <a href="home.html#sobre">Sobre</a> • 
+                <a href="projects.html">Meus Projetos</a> • 
+                <a href="tutorial.html">Tutorial</a>
+            </nav>
+            <p>&copy; 2025 Artephy - adicionar texto</p>
+        </footer>
         `
-<header id="DefaultHeader">
-    <img id="menu" src="header/menu.png" alt="Menu">
-    <img id="logo" src="favicon.ico" alt="Logo" >
-    <h1>Artephy</h1>
-</header>
-<div id="background-black"></div>
-<div id="BarraMenu">
-    <img id="LogoMenu" src="favicon.ico" alt="Logo">
-    <h1>Artephy</h1>
-    <ul>
-        <li><a href="home.html">HOME</a></li>
-        <li><a href="projects.html">SEUS PROJETOS</a></li>
-        <li><a href="sobre.html">SOBRE</a></li>
-        <li><a href="tutorial.html">TUTORIAL</a></li>
-    </ul>
-</div>
-`);
-})
+    )
+  );
+});
