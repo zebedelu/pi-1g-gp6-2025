abd94eb67c1b4cb65ddde930511312f33389cb0b
Pedro Vinicius <129136825+zebedelu@users.noreply.github.com>
2025-08-18 17:08:15 -0300

    Add files via upload

--- header/header.js
+++ header/header.js
@@ -3,21 +3,13 @@ document.addEventListener("DOMContentLoaded", () => {
     "afterbegin",
     `
     <header id="DefaultHeader">
-        <img id="menu" src="header/menu.png" alt="Menu">
-        <img id="logo" src="favicon.ico" alt="Logo" >
+        <img id="logo" src="favicon.ico" alt="Logo" onclick="window.location.href = 'home.html'">
         <h1>Artephy</h1>
+        <a href="home.html">Home</a>
+        <a href="home.html#sobre">Sobre</a>
+        <a href="projects.html">Meus Projetos</a>
+        <a href="tutorial.html">Tutorial</a>
     </header>
-    <div id="background-black"></div>
-    <div id="BarraMenu">
-        <img id="LogoMenu" src="favicon.ico" alt="Logo">
-        <h1>Artephy</h1>
-        <ul>
-            <li><a href="home.html">HOME</a></li>
-            <li><a href="home.html#sobre">SOBRE</a></li>
-            <li><a href="projects.html">SEUS PROJETOS</a></li>
-            <li><a href="tutorial.html">TUTORIAL</a></li>
-        </ul>
-    </div>
     `,
     document.body.querySelector("nav").insertAdjacentHTML(
       "beforeend",
